e58aa42da9f86f19e08babc937473c83a00c7c69
lillia05 <indahlialekok@gmail.com>
2024-03-09 14:30:22 +0700

    praktikum petruk

---
+++ 02 Array/arr1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double arr1[9];
+	int arr2[12];
+	
+    for(int i = 0; i < 9; i++){
+        cin >> arr1[i];
+    }
+
+    for(int i = 0; i < 9; i++){
+        cout << arr1[i];
+    }
+
+    for(int i = 0; i < 12; i++){
+        cin >> arr2[i];
+    }
+
+    for(int i = 0; i < 12; i++){
+        cout << arr2[i];
+    }
+
+return 0;
+}
